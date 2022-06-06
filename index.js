618127c908379a6b2c251b7a8d1311f64e14e008
Samson <samblint@gmail.com>
2022-06-06 17:18:52 +0300

    commit tree c866d4145ce213d492bf801ec41fd962b75f0bb4

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const companyName = `Scuber`;
+let mostProfitableNeighborhood = `Chelsea`;
+let companyCeo = `Susan Smith`;
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
